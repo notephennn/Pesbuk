5c9ad5ab30c8dd482a10e8bf900285c0f18c0556
notephennn <ephennn04@gmail.com>
2025-03-04 14:03:38 +0700

    perbaikan error

--- pesbuk.cpp
+++ pesbuk.cpp
@@ -14,7 +14,7 @@ typedef struct User {
     char username[MAX]; 
     char full_name[MAX]; 
     char birth_date[MAX]; 
-    Friend *friend; 
+    Friend *friends; 
     struct User *next; 
 } User; 
 
@@ -42,10 +42,10 @@ int isvalidUsername(const char *username){
 }
 
 int isValidFullName(const char *fullName){
-    int len = strlen(full_name);
+    int len = strlen(fullName);
     if (len < 1 || len > 50) return 0;
     for (int i = 0; i < len; i++) {
-        if (!isalpha(full_name[i]) && full_name[i] != ' ') return 0;
+        if (!isalpha(fullName[i]) && fullName[i] != ' ') return 0;
     }
     return 1;
 }
@@ -73,11 +73,11 @@ void menu_register(){
     do {
         printf("Enter username: ");
         scanf("%s", username);
-        if (!isValidUsername(username)) {
+        if (!isvalidUsername(username)) {
             printf("Invalid username! Must be 5-20 characters and only contain letters, numbers, or underscores.\n");
             continue;
         }
-        if (findUser(username)) {
+        if (user(username)) {
             printf("Username already taken!\n");
             continue;
         }
@@ -119,7 +119,7 @@ void menu_register(){
     strcpy(newUser->username, username);
     strcpy(newUser->full_name, full_name);
     strcpy(newUser->birth_date, birth_date);
-    newUser->friend = NULL;
+    newUser->friends = NULL;
     newUser->next = NULL;
 
 
@@ -136,7 +136,7 @@ void menu_register(){
 
 int countFriends(Friend *friendlist){
     int count = 0; 
-    while ( friendList ){
+    while ( friendlist ){
         count++; 
         friendlist = friendlist->next; 
     }
@@ -169,20 +169,20 @@ void menu_add() {
     char username[MAX], friendUsername[MAX], confirm;
     printf("Enter your username: ");
     scanf("%s", username);
-    User *user = findUser(username);
-    if (!user) {
+    User *newuser = user(username);
+    if (!newuser) {
         printf("User not found!\n");
         return;
     }
 
-    if (countFriends(user->friend) >= 10) {
+    if (countFriends(newuser->friends) >= 10) {
         printf("You already have too many friends\n");
         return;
     }
 
     printf("Enter username to befriend: ");
     scanf("%s", friendUsername);
-    User *friendUser = findUser(friendUsername);
+    User *friendUser = user(friendUsername);
     if (!friendUser) {
         printf("User not found!\n");
         return;
@@ -191,11 +191,11 @@ void menu_add() {
         printf("You cannot befriend yourself!\n");
         return;
     }
-    if (isAlreadyFriend(user->friend, friendUsername)) {
+    if (isAlreadyFriend(newuser->friends, friendUsername)) {
         printf("You are already friends with this user!\n");
         return;
     }
-    if (countFriends(friendUser->friend) >= 10) {
+    if (countFriends(friendUser->friends) >= 10) {
         printf("The user already has too many friends\n");
         return;
     }
@@ -207,18 +207,81 @@ void menu_add() {
         if (confirm == 'N') return;
     } while (confirm != 'Y');
 
-    addFriendToList(&user->friend, friendUsername);
-    addFriendToList(&friendUser->friend, username);
+    addFriendToList(&newuser->friends, friendUsername);
+    addFriendToList(&friendUser->friends, username);
 
     printf("Friend added successfully!\n");
 }
 
 void menu_show(){
-
+    char username[MAX];
+    printf("Enter your username: ");
+    scanf("%s", username);
+    User *newuser = user(username);
+    if (!newuser) {
+        printf("User not found!\n");
+        return;
+    }
+    if (!newuser->friends) {
+        printf("You have no friends yet\n");
+        return;
+    }
+    
+    // Collect friends' data
+    User *friends[10];
+    int count = 0;
+    Friend *temp = newuser->friends;
+    while (temp && count < 10) {
+        User *friendUser = user(temp->username);
+        if (friendUser) {
+            friends[count++] = friendUser;
+        }
+        temp = temp->next;
+    }
+    
+    // Sort friends by birthdate (dd/mm)
+    for (int i = 0; i < count - 1; i++) {
+        for (int j = 0; j < count - i - 1; j++) {
+            int day1, month1, year1;
+            int day2, month2, year2;
+            sscanf(friends[j]->birth_date, "%d/%d/%d", &day1, &month1, &year1);
+            sscanf(friends[j + 1]->birth_date, "%d/%d/%d", &day2, &month2, &year2);
+            if (month1 > month2 || (month1 == month2 && day1 > day2)) {
+                User *temp = friends[j];
+                friends[j] = friends[j + 1];
+                friends[j + 1] = temp;
+            }
+        }
+    }
+    
+    // Display sorted friends
+    printf("Friend Birthdays:\n");
+    for (int i = 0; i < count; i++) {
+        int day, month, year;
+        sscanf(friends[i]->birth_date, "%d/%d/%d", &day, &month, &year);
+        printf("Name: %s\n", friends[i]->full_name);
+        printf("Birthday: %02d/%02d\n", day, month);
+    }
 }
 
 void menu_delete(){
-
+    char username[MAX];
+    printf("Enter username to delete: ");
+    scanf("%s", username);
+    int index = midSquareHash(username);
+    User *prev = NULL, *temp = hashTable[index];
+    while (temp && strcmp(temp->username, username) != 0) {
+        prev = temp;
+        temp = temp->next;
+    }
+    if (!temp) {
+        printf("User not found!\n");
+        return;
+    }
+    if (prev) prev->next = temp->next;
+    else hashTable[index] = temp->next;
+    free(temp);
+    printf("Account deleted successfully!\n");
 }
 
 void clearHashTable(){
@@ -250,7 +313,7 @@ int main(){
 
         if ( option == 1 ) menu_register(); 
         else if ( option == 2 ) menu_add(); 
-        else if ( option == 3 ) menu_view(); 
+        else if ( option == 3 ) menu_show(); 
         else if ( option == 4) menu_delete(); 
         else if ( option == 5 ){
             clearHashTable(); 
